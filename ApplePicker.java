dc6a80bf2defc52d1f5d6f436651599142767558
Ronald Ewing <ronald@Ronalds-Mac-Pro.local>
2020-11-18 13:35:00 -0600

    Initial Commit

---
+++ ApplePicker.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+/*
+    @author Ronald Ewing
+    CSCI 1010-9 and 1011-1
+    Assignment 5
+
+        The program will allow a user to put
+        how many apples they have and get the
+        amount using java.util.Scanner and
+        System.out.println
+
+ */
+public class ApplePicker {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("How many apples did you pick?");
+
+        String userString = myScanner.nextLine();
+
+        int apples = Integer.parseInt(userString);
+
+
+        if(apples < 0) {
+            System.out.println("Number does not exist, please dont be Autisti");
+            return;
+        }
+
+        if (apples < 10 )
+        {
+            System.out.println("Total Charge is: " + apples*5);
+        }
+
+        if (apples >= 10 && apples <= 39 )
+        {
+            System.out.println("Total Charge is: " + apples*4.5);
+        }
+
+        if (apples >= 40 && apples <= 59 )
+        {
+            System.out.println("Total Charge is: " + apples*4);
+        }
+
+        if (apples > 60 )
+        {
+            System.out.println("Total Charge is: " + apples*3.5);
+        }
+
+        System.out.println("Goodbye!");
+
+    }
+}
